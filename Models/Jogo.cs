de461dbf6e95f6ca9cc9620ead254964c0c5d3d0
Joabe Alexandre <joabe.al@gmail.com>
2018-11-27 13:19:40 -0200

    Add models

---
+++ Models/Jogo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Jogo
+    {
+        public int Id { get; set; }
+        public string Inicio { get; set; }
+        public string Fim { get; set; } 
+    }
+}
